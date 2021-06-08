d3d97d334dbcd648e38b7504d2babeecfc07539a
pleasematheus <matheus4k@gmail.com>
2021-06-08 16:54:58 -0400

    Added a bit upgrade on My links

--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+import Menu from '../../components/Menu'
 import StatusBarPage from '../../components/StatusBarPage'
+import { Container, Title, ListLinks } from './styles'
+import ListItem from '../../components/ListItem'
 
 export default function MyLinks() {
 	return (
-		<View>
+		<Container>
 			<StatusBarPage
 				barStyle="light-content"
 				backgroundColor="#006666"
 			/>
-			<Text>
-				MyLinks
-			</Text>
-		</View>
+
+			<Menu/>
+			
+			<Title>Meus links</Title>
+
+			<ListLinks
+				data={[{ id: 1, link: 'teste' }]}
+				keyExtractor={(item) => String(item.id)}
+				renderItem={({ item }) => <ListItem data={item} />}
+				contentContainerStyle={{ paddingBottom: 20 }}
+				showsVerticalScrollIndicator={false}
+			/>
+			
+		</Container>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		width: '100%',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
-})
